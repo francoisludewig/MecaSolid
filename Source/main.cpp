0446532520e8cec6d231f0f88b8c83c1bf0aed2b
Ludewig François <francoisludewig@gmail.com>
2016-03-29 21:21:43 +0200

    Correction

---
+++ Source/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "Basis/Vector3D.h"
+#include "Basis/UnitQuaternion.h"
+
+using namespace std;
+using namespace Meca::Libs::Basis;
+
+int main()
+{
+	int x;
+
+	Vector3D a;
+	a.SetValue(1,2,3);
+	cout << a << endl;
+
+	UnitQuaternion b;
+	b.SetValue(1,1,1,1,true);
+	cout << b << endl;
+
+	cout << "Enter an integer value : ";
+    cin >> x;
+    return 0;
+}
